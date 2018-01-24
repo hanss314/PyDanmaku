6210458fbe9cc67f1f268d53508b5fb9fb76b75f
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-01-23 18:54:19 -0800

    PyPI setup stuff

--- pydanmaku/__init__.py
+++ pydanmaku/__init__.py
@@ -1,3 +1,8 @@
 from .game import Game
 from .player import Player
 from .bullet import Bullet
+
+__title__ = 'pydanmaku'
+__author__ = 'hanss'
+__license__ = 'MIT'
+__version__ = '0.0.0-alpha'
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import re
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('pydanmaku/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='pydanmaku',
+      author='hanss',
+      url='https://github.com/hanss314/PyDanmaku',
+      version=version,
+      packages=['pydanmaku'],
+      license='MIT',
+      description='A Danmaku engine written in python',
+      long_description=readme,
+      include_package_data=True,
+      install_requires=requirements,
+)
