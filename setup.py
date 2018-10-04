225e0a971534f829d0dd5b031fc19e5156ffc289
skripley <ripleyskylar@gmail.com>
2018-10-03 18:34:15 -0700

    Moves version declaration to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
 import re
 
+version = '0.0.1a1'
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-
-with open('pydanmaku/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError('version is not set')
 
 with open('README.md') as f:
     readme = f.read()
